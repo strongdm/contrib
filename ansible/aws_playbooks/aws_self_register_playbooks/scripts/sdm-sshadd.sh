8db87568ba0395870afa59696f40b359c92330b0
John Turner <jturner@b1tsized.tech>
2021-06-28 13:28:07 -0500

    Adding in some Ansible examples for SDM and AWS

---
+++ ansible/aws_playbooks/aws_self_register_playbooks/scripts/sdm-sshadd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ export SDM_ADMIN_TOKEN= {{ SDM_ADMIN_TOKEN }}
+ apt update
+ apt install -y unzip
+ curl -o sdm.zip -L https://app.strongdm.com/releases/cli/linux
+ unzip sdm.zip
+ ./sdm admin ssh add \
+   -p `curl http://169.254.169.254/latest/meta-data/instance-id` \
+   $USERNAME@`curl http://169.254.169.254/latest/meta-data/public-hostname` \
+   | tee -a "/home/$USERNAME/.ssh/authorized_keys"
+ ./sdm admin roles grant `curl http://169.254.169.254/latest/meta-data/instance-id` {{ SDM_Role }}
+ rm sdm.zip
